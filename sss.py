0d0ba6e089ae3442fe656830a9f8bbc31348afae
johnn7676 <unknowingly0612@gmail.com>
2026-01-08 11:32:36 +0500

    yangi narsa qushdik 8-yanvarda

---
+++ sss.py
@@ -0,0 +1,13 @@
+for i in range(3, 17, 3):
+    print(i)
+
+print("///////////////")
+
+for i in range(20, 5, -1):
+    print(i)
+
+print("///////////////")
+a = "qwertyuiop"
+for i in a:
+    print(i)
+
